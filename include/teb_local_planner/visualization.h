d8e986146babd04f0b8600c8c438b87655b018a9
spahnit <thedecipher.sp@gmail.com>
2020-02-20 15:03:27 +0100

    Added time to Goal

--- include/teb_local_planner/visualization.h
+++ include/teb_local_planner/visualization.h
@@ -45,6 +45,7 @@
 #include <teb_local_planner/teb_config.h>
 #include <teb_local_planner/timed_elastic_band.h>
 #include <teb_local_planner/robot_footprint_model.h>
+#include <teb_local_planner/TrajectoryMsg.h>
 
 // ros stuff
 #include <ros/publisher.h>
@@ -64,14 +65,15 @@
 #include <tf/transform_datatypes.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
+#include "metrics/TimeToGoal.h"
 #include <visualization_msgs/Marker.h>
 
 namespace teb_local_planner
 {
-  
-class TebOptimalPlanner; //!< Forward Declaration 
 
-  
+class TebOptimalPlanner; //!< Forward Declaration
+
+
 /**
  * @class TebVisualization
  * @brief Visualize stuff from the teb_local_planner
@@ -79,76 +81,76 @@ class TebOptimalPlanner; //!< Forward Declaration
 class TebVisualization
 {
 public:
-    
+
   /**
    * @brief Default constructor
    * @remarks do not forget to call initialize()
    */
   TebVisualization();
-  
+
   /**
    * @brief Constructor that initializes the class and registers topics
    * @param nh local ros::NodeHandle
    * @param cfg const reference to the TebConfig class for parameters
    */
   TebVisualization(ros::NodeHandle& nh, const TebConfig& cfg);
-  
+
   /**
    * @brief Initializes the class and registers topics.
-   * 
+   *
    * Call this function if only the default constructor has been called before.
    * @param nh local ros::NodeHandle
    * @param cfg const reference to the TebConfig class for parameters
    */
   void initialize(ros::NodeHandle& nh, const TebConfig& cfg);
-  
-  
+
+
   /** @name Publish to topics */
   //@{
-  
+
   /**
    * @brief Publish a given global plan to the ros topic \e ../../global_plan
    * @param global_plan Pose array describing the global plan
    */
   void publishGlobalPlan(const std::vector<geometry_msgs::PoseStamped>& global_plan) const;
-  
+
   /**
    * @brief Publish a given local plan to the ros topic \e ../../local_plan
    * @param local_plan Pose array describing the local plan
    */
   void publishLocalPlan(const std::vector<geometry_msgs::PoseStamped>& local_plan) const;
-  
+
   /**
    * @brief Publish Timed_Elastic_Band related stuff (local plan, pose sequence).
-   * 
-   * Given a Timed_Elastic_Band instance, publish the local plan to  \e ../../local_plan 
+   *
+   * Given a Timed_Elastic_Band instance, publish the local plan to  \e ../../local_plan
    * and the pose sequence to  \e ../../teb_poses.
    * @param teb const reference to a Timed_Elastic_Band
    */
   void publishLocalPlanAndPoses(const TimedElasticBand& teb) const;
-  
+
   /**
    * @brief Publish the visualization of the robot model
-   * 
+   *
    * @param current_pose Current pose of the robot
    * @param robot_model Subclass of BaseRobotFootprintModel
    * @param ns Namespace for the marker objects
    */
   void publishRobotFootprintModel(const PoseSE2& current_pose, const BaseRobotFootprintModel& robot_model, const std::string& ns = "RobotFootprintModel");
-  
+
   /**
    * @brief Publish obstacle positions to the ros topic \e ../../teb_markers
    * @todo Move filling of the marker message to polygon class in order to avoid checking types.
    * @param obstacles Obstacle container
    */
   void publishObstacles(const ObstContainer& obstacles) const;
-  
+
   /**
    * @brief Publish via-points to the ros topic \e ../../teb_markers
    * @param via_points via-point container
    */
   void publishViaPoints(const std::vector< Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d> >& via_points, const std::string& ns = "ViaPoints") const;
-  
+
   /**
    * @brief Publish a boost::adjacency_list (boost's graph datatype) via markers.
    * @remarks Make sure that vertices of the graph contain a member \c pos as \c Eigen::Vector2d type
@@ -159,17 +161,17 @@ public:
    */
   template <typename GraphType>
   void publishGraph(const GraphType& graph, const std::string& ns_prefix = "Graph");
-  
+
   /**
    * @brief Publish multiple 2D paths (each path given as a point sequence) from a container class.
-   * 
+   *
    * Provide a std::vector< std::vector< T > > in which T.x() and T.y() exist
    * and std::vector could be individually substituded by std::list / std::deque /...
-   * 
+   *
    * A common point-type for object T could be Eigen::Vector2d.
-   * 
+   *
    * T could be also a raw pointer std::vector< std::vector< T* > >.
-   * 
+   *
    * @code
    * 	typedef std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d> > PathType; // could be a list or deque as well ...
    *    std::vector<PathType> path_container(2); // init 2 empty paths; the container could be a list or deque as well ...
@@ -177,8 +179,8 @@ public:
    * 	// Fill path_container.at(1) with Eigen::Vector2d elements, we skip that here
    *    publishPathContainer( path_container.begin(), path_container.end() );
    * @endcode
-   * 
-   * @remarks Actually the underlying path does not necessarily need to be a Eigen::Vector2d sequence. 
+   *
+   * @remarks Actually the underlying path does not necessarily need to be a Eigen::Vector2d sequence.
    *          Eigen::Vector2d can be replaced with any datatype that implement public x() and y() methods.\n
    * @param first Bidirectional iterator pointing to the begin of the path
    * @param last Bidirectional iterator pointing to the end of the path
@@ -187,18 +189,20 @@ public:
    */
   template <typename BidirIter>
   void publishPathContainer(BidirIter first, BidirIter last, const std::string& ns = "PathContainer");
-  
+
   /**
    * @brief Publish multiple Tebs from a container class (publish as marker message).
-   * 
+   *
    * @param teb_planner Container of boost::shared_ptr< TebOptPlannerPtr >
    * @param ns Namespace for the marker objects
    */
   void publishTebContainer(const std::vector< boost::shared_ptr<TebOptimalPlanner> >& teb_planner, const std::string& ns = "TebContainer");
-    
+
+
+  void publishTrajectory(const std::vector<TrajectoryPointMsg>& trajectory) const;
   /**
    * @brief Publish a feedback message (multiple trajectory version)
-   * 
+   *
    * The feedback message contains the all planned trajectory candidates (e.g. if planning in distinctive topologies is turned on).
    * Each trajectory is composed of the sequence of poses, the velocity profile and temporal information.
    * The feedback message also contains a list of active obstacles.
@@ -207,10 +211,10 @@ public:
    * @param obstacles Container of obstacles
    */
   void publishFeedbackMessage(const std::vector< boost::shared_ptr<TebOptimalPlanner> >& teb_planners, unsigned int selected_trajectory_idx, const ObstContainer& obstacles);
-  
+
   /**
    * @brief Publish a feedback message (single trajectory overload)
-   * 
+   *
    * The feedback message contains the planned trajectory
    * that is composed of the sequence of poses, the velocity profile and temporal information.
    * The feedback message also contains a list of active obstacles.
@@ -218,11 +222,11 @@ public:
    * @param obstacles Container of obstacles
    */
   void publishFeedbackMessage(const TebOptimalPlanner& teb_planner, const ObstContainer& obstacles);
-  
+
   //@}
-  
+
 protected:
-  
+
   /**
    * @brief Small helper function that checks if initialize() has been called and prints an error message if not.
    * @return \c true if not initialized, \c false if everything is ok
@@ -234,14 +238,15 @@ protected:
   ros::Publisher teb_poses_pub_; //!< Publisher for the trajectory pose sequence
   ros::Publisher teb_marker_pub_; //!< Publisher for visualization markers
   ros::Publisher feedback_pub_; //!< Publisher for the feedback message for analysis and debug purposes
-  
+  ros::Publisher robot_traj_time_pub_, robot_path_time_pub_;
+
   const TebConfig* cfg_; //!< Config class that stores and manages all related parameters
-  
+
   bool initialized_; //!< Keeps track about the correct initialization of this class
 
-    
+
 public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW    
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
 
 //! Abbrev. for shared instances of the TebVisualization
